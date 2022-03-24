8c46a66d42a9c69c837e6b594812a47199fd1476
firas khalil <firaskhalilkhalil@gmail.com>
2022-03-24 19:29:34 +0300

    Second Edit

---
+++ Hafta1/The_Valume_Of-Cylinder/The_Valume_Of-Cylinder/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter The radius of the base of the cylinder by cm: ");
+int r = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter  cylinder height by cm: ");
+int h = Convert.ToInt32(Console.ReadLine());
+
+double v = 3.14 * (r * r) * h;
+
+Console.WriteLine("The size of the cylinder is : "+v+ " cm3");
+
+
+
